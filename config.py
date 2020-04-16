fa5f73c8ba2e925c8c96ec8e5f594354c018b3f1
ArianaRuggiero <60370127+ArianaRuggiero@users.noreply.github.com>
2020-04-16 12:17:09 -0400

    TASK: Added config.py file

---
+++ config.py
@@ -0,0 +1,49 @@
+import flask_calendar.constants as constants
+
+DEBUG = True
+DATA_FOLDER = "data"
+USERS_DATA_FOLDER = "users"
+BASE_URL = "http://127.0.0.1:5000"
+MIN_YEAR = 2017
+MAX_YEAR = 2100
+PASSWORD_SALT = "something random and full of non-standard characters"
+HOST_IP = "0.0.0.0"  # set to None for production
+LOCALE = "es_ES.UTF-8"
+# https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+TIMEZONE = "Europe/Madrid"
+
+WEEK_STARTING_DAY = constants.WEEK_START_DAY_SUNDAY
+
+MONTHS_TO_EXPORT = 6  # currently only used for ICS export
+
+FEATURE_FLAG_ICAL_EXPORT = False
+
+# (base ^ attempts ) second delays between failed logins
+FAILED_LOGIN_DELAY_BASE = 2
+
+# If true, will automatically decorate hyperlinks with <a> tags upon rendering them
+AUTO_DECORATE_TASK_DETAILS_HYPERLINK = True
+
+SHOW_VIEW_PAST_BUTTON = True
+
+# Of use if SHOW_VIEW_PAST_BUTTON is False
+HIDE_PAST_TASKS = False
+
+# days past to keep hidden tasks (future ones always kept) counting all months as 31 days long
+DAYS_PAST_TO_KEEP_HIDDEN_TASKS = 62
+
+# Cookies config
+COOKIE_HTTPS_ONLY = False
+COOKIE_SAMESITE_POLICY = "Lax"
+
+# If to render emoji buttons at the task create/edit page
+EMOJIS_ENABLED = True
+
+# Colors for new task buttons
+BUTTON_CUSTOM_COLOR_VALUE = "#2F06F8"
+BUTTONS_COLORS_LIST = (
+    ("#FF4848", "Red"), ("#3EB34F", "Green"), ("#2966B8", "Blue"), ("#FFFF00", "Yellow"), ("#FFC0CB", "Pink"),
+    ("#9588EC", "Purple"), ("#F2981A", "Orange"), ("#7FFFD4", "Aqua")
+)
+# Emojis for new task buttons
+BUTTONS_EMOJIS_LIST = ("💬", "📞", "🍔", "🍺", "📽️", "🎂", "🏖️", "💻", "📔", "✂️", "🚂", "🏡", "🐶", "🐱")
